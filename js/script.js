9239a2e4fab95627f983c7224832e0b93c28a7a2
639dev <gharood_3@hotmail.com>
2018-02-04 08:16:55 +0300

    share data between ko and map script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
       var map;
       var markers = [];
       function initMap() {
@@ -6,23 +7,41 @@
           center: {lat: 24.643333, lng: 46.695831}
         });
 
-        
-        var locations = [
+        // var locations = [
+        //   {title: 'Centeria Mall', location: {lat: 24.698415, lng: 46.683869}},
+        //   {title: 'Kingdom Tower', location: {lat: 24.7114, lng: 46.6744}},
+        //   {title: 'Burger Boutique', location: {lat: 24.703426, lng: 46.678498}},
+        //   {title: 'Jarir Bookstore', location: {lat: 24.702556, lng: 46.680667}},
+        //   {title: 'Aqariyah Plaza', location: {lat: 24.708017, lng: 46.678737}}
+        // ];
+
+
+        // Overall viewmodel for this screen, along with initial state
+        function PlaceViewModel() {
+          var self = this;  
+
+          // Editable data
+          self.locations = ko.observableArray([
           {title: 'Centeria Mall', location: {lat: 24.698415, lng: 46.683869}},
           {title: 'Kingdom Tower', location: {lat: 24.7114, lng: 46.6744}},
           {title: 'Burger Boutique', location: {lat: 24.703426, lng: 46.678498}},
           {title: 'Jarir Bookstore', location: {lat: 24.702556, lng: 46.680667}},
           {title: 'Aqariyah Plaza', location: {lat: 24.708017, lng: 46.678737}}
-        ];
+          ]);
+        }
+
+        var viewModel = new PlaceViewModel();
+        ko.applyBindings(viewModel);
+
 
         var largeInfowindow = new google.maps.InfoWindow();
         var bounds = new google.maps.LatLngBounds();
         var highlightedIcon = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
         var defaultIcon = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
 
-        for (var i = 0; i < locations.length; i++) {
-          var position = locations[i].location;
-          var title = locations[i].title;
+        for (var i = 0; i < viewModel.locations().length; i++) {
+          var position = viewModel.locations()[i].location;
+          var title = viewModel.locations()[i].title;
           //create marker per location,then put in markers array
           var marker = new google.maps.Marker({
             position: position,
